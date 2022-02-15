52d07a48cf130aa66bb0e00cfeb9eb5c12e84c59
jasconchan <jasonchenzehui@gmail.com>
2022-02-15 21:04:34 +0800

    deploy

--- assets/quickstart.6440c832.js
+++ assets/quickstart.6440c832.js
@@ -1,4 +1,4 @@
-import{o as a,i as n,j as l}from"./vendor.2fdeaaab.js";const c={class:"v-doc-content"},p=l(`<div class="v-doc-card"><h3>\u4ECB\u7ECD</h3><p>\u901A\u8FC7\u672C\u7AE0\u8282\u4F60\u53EF\u4EE5\u4E86\u89E3\u5230 @vcomponetns/vant-ui \u7684\u5B89\u88C5\u65B9\u6CD5\u548C\u57FA\u672C\u4F7F\u7528\u59FF\u52BF\u3002</p></div><div class="v-doc-card"><h3>\u524D\u7F6E\u6761\u4EF6</h3><pre><code class="language-js"><span class="hljs-comment">// main.js</span>
+import{o as a,i as n,m as l}from"./vendor.a0f7cb7c.js";const c={class:"v-doc-content"},p=l(`<div class="v-doc-card"><h3>\u4ECB\u7ECD</h3><p>\u901A\u8FC7\u672C\u7AE0\u8282\u4F60\u53EF\u4EE5\u4E86\u89E3\u5230 @vcomponetns/vant-ui \u7684\u5B89\u88C5\u65B9\u6CD5\u548C\u57FA\u672C\u4F7F\u7528\u59FF\u52BF\u3002</p></div><div class="v-doc-card"><h3>\u524D\u7F6E\u6761\u4EF6</h3><pre><code class="language-js"><span class="hljs-comment">// main.js</span>
 <span class="hljs-comment">// \u5EFA\u8BAE\u5168\u5C40\u5F15\u5165</span>
 <span class="hljs-keyword">import</span> <span class="hljs-title class_">Vue</span> <span class="hljs-keyword">from</span> <span class="hljs-string">&#39;vue&#39;</span>
 
@@ -6,7 +6,7 @@ import{o as a,i as n,j as l}from"./vendor.2fdeaaab.js";const c={class:"v-doc-con
 <span class="hljs-keyword">import</span> <span class="hljs-string">&#39;vant/lib/index.css&#39;</span>
 
 <span class="hljs-title class_">Vue</span>.<span class="hljs-title function_">use</span>(<span class="hljs-title class_">Vant</span>)
-</code></pre></div><div class="v-doc-card"><h3>\u5B89\u88C5</h3><pre><code class="language-shell">npm i vantui-components -S
+</code></pre></div><div class="v-doc-card"><h3>\u5B89\u88C5</h3><pre><code class="language-bash">npm i vantui-components -S
 </code></pre></div><div class="v-doc-card"><h3>\u5F15\u5165\u7EC4\u4EF6</h3><pre><code class="language-js"><span class="hljs-comment">// main.js</span>
 <span class="hljs-comment">// \u5168\u5C40\u5F15\u5165</span>
 <span class="hljs-keyword">import</span> <span class="hljs-title class_">VComponents</span> <span class="hljs-keyword">from</span> <span class="hljs-string">&#39;vantui-components&#39;</span>
