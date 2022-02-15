aec8556dbd1cb742cc23180da30b3104b1801028
jasconchan <jasonchenzehui@gmail.com>
2022-02-15 21:37:24 +0800

    feat: 文档增加加载进度条

--- src/router/guard.js
+++ src/router/guard.js
@@ -1,16 +1,23 @@
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
 import getPageTitle from '@/utils/get-page-title'
+
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 export function setupRouterGuard(router) {
   router.beforeEach(async (to, from) => {
+    // start progress bar
+    NProgress.start()
     // set page title
     document.title = getPageTitle(to.meta.title)
   })
 
   router.afterEach((to, from) => {
-
+    NProgress.done()
   })
 
   router.onError((error) => {
     console.log(error, '路由错误')
+    NProgress.done()
   })
 }
