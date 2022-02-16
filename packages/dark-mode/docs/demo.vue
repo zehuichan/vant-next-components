e47da06e00e44ac9263f66c3e5ea266e66dbaf51
zehuichan <jasonchenzehui@gmail.com>
2022-02-16 14:09:41 +0800

    fix

---
+++ packages/dark-mode/docs/demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <demo-card title="Button 按钮" class="demo-button">
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </demo-card>
+  <demo-card title="Cell 单元格">
+    <van-cell-group title="分组1">
+      <van-cell title="单元格" value="内容"/>
+    </van-cell-group>
+    <van-cell-group title="分组2">
+      <van-cell title="单元格" value="内容"/>
+    </van-cell-group>
+    <van-cell-group title="分组3">
+      <van-cell value="内容"/>
+      <van-cell title="单元格" is-link/>
+    </van-cell-group>
+  </demo-card>
+</template>
+
+<style lang="less">
+.demo-button {
+  .van-button + .van-button {
+    margin-left: 10px;
+  }
+}
+</style>
