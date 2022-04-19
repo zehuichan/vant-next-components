fdbab7d7b607d0d33551175a8cc2d10cbe05f838
zehuichan <jasonchenzehui@gmail.com>
2022-04-19 17:51:32 +0800

    feat: space间隔组件

---
+++ packages/space/Space.jsx
@@ -0,0 +1,107 @@
+import { defineComponent, computed } from 'vue'
+import { createNamespace, isArray, isNumber, filterEmpty } from '../utils'
+
+const [name] = createNamespace('space')
+
+import './index.less'
+
+export default defineComponent({
+  name,
+  props: {
+    align: String,
+    direction: {
+      type: String,
+      default: 'horizontal',
+    },
+    size: {
+      type: [Number, String, Array],
+      default: 'small',
+    },
+    wrap: {
+      type: Boolean,
+      default: false
+    },
+    fill: {
+      type: Boolean,
+    },
+  },
+  setup(props, { slots }) {
+    const mergedAlign = computed(
+      () => props.align ?? (props.direction === 'horizontal' ? 'center' : '')
+    )
+
+    const cls = computed(() => [
+      name,
+      {
+        [`${name}-${props.direction}`]: props.direction,
+        [`${name}-align-${mergedAlign.value}`]: mergedAlign.value,
+        [`${name}-wrap`]: props.wrap,
+        [`${name}-fill`]: props.fill,
+      },
+    ])
+
+    function getMargin(size) {
+      if (isNumber(size)) {
+        return size
+      }
+      switch (size) {
+        case 'mini':
+          return 4
+        case 'small':
+          return 8
+        case 'medium':
+          return 16
+        case 'large':
+          return 24
+        default:
+          return 8
+      }
+    }
+
+    const getMarginStyle = (index, isLast) => {
+      const style = {}
+
+      const marginRight = `${getMargin(
+        isArray(props.size) ? props.size[0] : props.size
+      )}px`
+      const marginBottom = `${getMargin(
+        isArray(props.size) ? props.size[1] : props.size
+      )}px`
+
+      if (isLast) {
+        return props.wrap ? { marginBottom } : {}
+      }
+
+      if (props.direction === 'horizontal') {
+        style.marginRight = marginRight
+      }
+      if (props.direction === 'vertical' || props.wrap) {
+        style.marginBottom = marginBottom
+      }
+
+      return style
+    }
+
+    return () => {
+      const items = filterEmpty(slots.default?.())
+      const isLast = items.length === 1
+
+      return (
+        <div class={cls.value}>
+          {
+            items.map((child, index) => (
+              <div
+                key={index}
+                class={`${name}-item`}
+                style={getMarginStyle(index, isLast)}
+              >
+                {child}
+              </div>
+            ))
+          }
+        </div>
+      )
+    }
+  }
+
+})
---
+++ packages/space/docs/demo.vue
@@ -0,0 +1,56 @@
+<template>
+  <demo-card>
+    <div class="slider-wrapper">
+      <van-slider v-model="size">
+        <template #button>
+          <div class="custom-button">{{ size }}</div>
+        </template>
+      </van-slider>
+    </div>
+    <v-space :size="[size, size]">
+     <v-card v-for="i in 3" :key="i" class="box-card" style="width: 250px">
+        <template #header>
+          <div class="card-header">
+            <span>Card name</span>
+          </div>
+        </template>
+        <div v-for="o in 4" :key="o" class="text item">
+          {{ 'List item ' + o }}
+        </div>
+      </v-card>
+    </v-space>
+  </demo-card>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+import VSpace from '..'
+import VCard from '../../card'
+
+const size = ref(20)
+</script>
+
+<style scoped>
+.slider-wrapper {
+  padding: 8px 0 24px;
+}
+
+.item {
+  margin-bottom: 12px;
+}
+
+.text {
+  font-size: 12px;
+}
+
+.custom-button {
+  width: 26px;
+  color: #fff;
+  font-size: 10px;
+  line-height: 18px;
+  text-align: center;
+  background-color: #1989fa;
+  border-radius: 100px;
+}
+</style>
